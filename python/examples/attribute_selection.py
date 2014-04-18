d8e5710daa978a33c06eec4d1915096093dcf548
Peter Reutemann <fracpete@gmail.com>
2014-04-19 11:55:05 +1200

    started on attribute selection module

---
+++ python/examples/attribute_selection.py
@@ -0,0 +1,43 @@
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# attribute_selection.py
+# Copyright (C) 2014 Fracpete (fracpete at gmail dot com)
+
+import os
+import weka.core.jvm as jvm
+import examples.helper as helper
+from weka.core.converters import Loader
+
+
+def main():
+    """
+    Just runs some example code.
+    """
+
+    # load a dataset
+    iris_file = helper.get_data_dir() + os.sep + "iris.arff"
+    helper.print_info("Loading dataset: " + iris_file)
+    loader = Loader("weka.core.converters.ArffLoader")
+    iris_data = loader.load_file(iris_file)
+    iris_data.set_class_index(iris_data.num_attributes() - 1)
+
+
+if __name__ == "__main__":
+    try:
+        jvm.start()
+        main()
+    except Exception, e:
+        print(e)
+    finally:
+        jvm.stop()
