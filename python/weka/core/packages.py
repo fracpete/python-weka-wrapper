fc38725cd4e9f08beae62ad04a92c7e0fe8032c6
Peter Reutemann <fracpete@gmail.com>
2014-05-01 22:46:24 +1200

    basic package support (install/uninstall still fails)

--- python/weka/core/packages.py
+++ python/weka/core/packages.py
@@ -15,4 +15,282 @@
 # Copyright (C) 2014 Fracpete (fracpete at gmail dot com)
 
 import javabridge
+import weka.core.jvm as jvm
+from weka.core.classes import JavaObject
 
+
+class Package(JavaObject):
+    """
+    Wrapper for the org.pentaho.packageManagement.Package class.
+    """
+
+    def __init__(self, jobject):
+        """
+        Initializes the wrapper.
+        :param jobject: the java Package instance to wrap
+        :type jobject: JB_Object
+        """
+        self.enforce_type(jobject, "org.pentaho.packageManagement.Package")
+        super(Package, self).__init__(jobject)
+
+    def get_name(self):
+        """
+        Returns the name of the package.
+        :return: the name
+        :rtype: str
+        """
+        return javabridge.call(self.jobject, "getName", "()Ljava/lang/String;")
+
+    def get_url(self):
+        """
+        Returns the URL of the package.
+        :return: the url
+        :rtype: str
+        """
+        return str(JavaObject(javabridge.call(self.jobject, "getPackageURL", "()Ljava/net/URL;")))
+
+    def get_dependencies(self):
+        """
+        Returns the dependencies of the package.
+        :return: the list of Dependency objects
+        :rtype: list of Dependency
+        """
+        result       = []
+        dependencies = javabridge.get_collection_wrapper(
+            javabridge.call(self.jobject, "getDependencies", "()Ljava/util/List;"))
+        for dependency in dependencies:
+            result.append(Dependency(dependency))
+        return result
+
+    def get_metadata(self):
+        """
+        Returns the meta-data.
+        :return: the meta-data dictionary
+        :rtype: dict
+        """
+        return javabridge.get_dictionary_wrapper(
+            javabridge.call(self.jobject, "getPackageMetaData", "()Ljava/util/Map;"))
+
+    def is_installed(self):
+        """
+        Returns whether the package is installed.
+        :return: whether installed
+        :rtype: bool
+        """
+        return javabridge.call(self.jobject, "isInstalled", "()Z")
+
+    def install(self):
+        """
+        Installs the package.
+        """
+        return javabridge.call(self.jobject, "install", "()V")
+
+
+class PackageConstraint(JavaObject):
+    """
+    Wrapper for the org.pentaho.packageManagement.PackageConstraint class.
+    """
+
+    def __init__(self, jobject):
+        """
+        Initializes the wrapper.
+        :param jobject: the java PackageConstraint instance to wrap
+        :type jobject: JB_Object
+        """
+        self.enforce_type(jobject, "org.pentaho.packageManagement.PackageConstraint")
+        super(PackageConstraint, self).__init__(jobject)
+
+    def set_package(self, pkg):
+        """
+        Sets the package.
+        :param pkg: the package
+        :type pkg: Package
+        """
+        javabridge.call(self.jobject, "setPackage", "(Lorg/pentaho/packageManagement/Package;)V", pkg.jobject)
+
+    def get_package(self):
+        """
+        Returns the package.
+        :return: the package
+        :rtype: Package
+        """
+        return Package(javabridge.call(self.jobject, "getPackage", "()Lorg/pentaho/packageManagement/Package;"))
+
+    def check_constraint(self, pkg=None, constr=None):
+        """
+        Checks the constraing.
+        :param pkg: the package to check
+        :type pkg: Package
+        :param constr: the package constraint to check
+        :type constr: PackageConstraint
+        """
+        if not pkg is None:
+            return javabridge.call(
+                self.jobject, "checkConstraint", "(Lorg/pentaho/packageManagement/Package;)Z", pkg.jobject)
+        if not constr is None:
+            return javabridge.call(
+                self.jobject, "checkConstraint", "(Lorg/pentaho/packageManagement/PackageConstraint;)Z", pkg.jobject)
+        raise Exception("Either package or package constraing must be provided!")
+
+
+class Dependency(JavaObject):
+    """
+    Wrapper for the org.pentaho.packageManagement.Dependency class.
+    """
+
+    def __init__(self, jobject):
+        """
+        Initializes the wrapper.
+        :param jobject: the java Dependency instance to wrap
+        :type jobject: JB_Object
+        """
+        self.enforce_type(jobject, "org.pentaho.packageManagement.Dependency")
+        super(Dependency, self).__init__(jobject)
+
+    def set_source(self, pkg):
+        """
+        Sets the source package.
+        :param pkg: the package
+        :type pkg: Package
+        """
+        javabridge.call(
+            self.jobject, "setSource", "(Lorg/pentaho/packageManagement/Package;)V", pkg.jobject)
+
+    def get_source(self):
+        """
+        Returns the source package.
+        :return: the package
+        :rtype: Package
+        """
+        return Package(
+            javabridge.call(self.jobject, "getSource", "()Lorg/pentaho/packageManagement/Package;"))
+
+    def set_target(self, constr):
+        """
+        Sets the target package constraint.
+        :param constr: the package constraint
+        :type constr: Package
+        """
+        javabridge.call(
+            self.jobject, "setTarget", "(Lorg/pentaho/packageManagement/PackageConstraint;)V", constr.jobject)
+
+    def get_target(self):
+        """
+        Returns the target package constraint.
+        :return: the package constraint
+        :rtype: PackageConstraint
+        """
+        return PackageConstraint(
+            javabridge.call(self.jobject, "getTarget", "()Lorg/pentaho/packageManagement/PackageConstraint;"))
+
+
+def get_all_packages():
+    """
+    Returns a list of all packages.
+    :return: the list of packages
+    :rtype: list
+    """
+    result = []
+    pkgs   = javabridge.get_collection_wrapper(
+        javabridge.static_call(
+            "weka/core/WekaPackageManager", "getAllPackages", "()Ljava/util/List;"))
+    for p in pkgs:
+        result.append(Package(p))
+    return result
+
+
+def get_available_packages():
+    """
+    Returns a list of all packages that aren't installed yet.
+    :return: the list of packages
+    :rtype: list
+    """
+    result = []
+    pkgs   = javabridge.get_collection_wrapper(
+        javabridge.static_call(
+            "weka/core/WekaPackageManager", "getAvailablePackages", "()Ljava/util/List;"))
+    for p in pkgs:
+        result.append(Package(p))
+    return result
+
+
+def get_installed_packages():
+    """
+    Returns a list of the installed packages.
+    :return: the list of packages
+    :rtype: list
+    """
+    result = []
+    pkgs   = javabridge.get_collection_wrapper(
+        javabridge.static_call(
+            "weka/core/WekaPackageManager", "getInstalledPackages", "()Ljava/util/List;"))
+    for p in pkgs:
+        result.append(Package(p))
+    return result
+
+
+def install_packages(pkgs):
+    """
+    The list of packages to install.
+    :param pkgs: the list of packages
+    :param pkgs: list
+    :return: whether successfully installed
+    :rtype: bool
+    """
+    lst = []
+    for p in pkgs:
+        lst.append(p.jobject)
+    jpkgs = javabridge.make_list(lst)
+    return javabridge.static_call(
+        "weka/core/WekaPackageManager", "installPackages", "(Ljava/util/List;[Ljava/io/PrintStream;)Z", jpkgs, [])
+
+
+def uninstall_package(name):
+    """
+    Uninstalls a package.
+    :param name: the name of the package
+    :param name: str
+    :return: whether successfully uninstalled
+    :rtype: bool
+    """
+    return javabridge.static_call(
+        "weka/core/WekaPackageManager", "uninstallPackage", "(Ljava/lang/String;Z[Ljava/io/PrintStream;)V", p.jobject, False, [])
+
+if __name__ == "__main__":
+    try:
+        jvm.start()
+
+        print("All packages")
+        print("============")
+        packages = get_all_packages()
+        for pkg in packages:
+            print(pkg.get_name())
+            print("  url: " + pkg.get_url())
+            print("")
+
+        print("Available packages")
+        print("==================")
+        packages = get_available_packages()
+        p = packages[0]
+        for pkg in packages:
+            print(pkg.get_name())
+            print("  url: " + pkg.get_url())
+            print("")
+
+        print("Installed packages")
+        print("==================")
+        packages = get_installed_packages()
+        for pkg in packages:
+            print(pkg.get_name())
+            print("  url: " + pkg.get_url())
+            print("")
+
+        print("Install: " + p.get_name())
+        print(install_packages([p]))
+
+        print("Uninstall: " + p.get_name())
+        print(uninstall_package(p.get_name()))
+
+        jvm.stop()
+    except Exception, e:
+        print(e)
