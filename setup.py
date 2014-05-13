f5ceab111b9015efbe99784e70e85822df35a0ed
Peter Reutemann <fracpete@gmail.com>
2014-05-13 16:58:00 +1200

    incremented versions

--- setup.py
+++ setup.py
@@ -94,7 +94,7 @@ setup(
         "wekaexamples": ["data/*.arff"],
     },
     include_package_data=True,
-    version="0.1.1",
+    version="0.1.2",
     author='Peter "fracpete" Reutemann',
     author_email='fracpete at gmail dot com',
     install_requires=[
