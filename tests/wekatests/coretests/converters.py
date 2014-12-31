1bf01f2c79e179dfa8faa98c3812674c4470ec00
Peter Reutemann <fracpete@gmail.com>
2014-12-31 15:19:36 +1300

    added tests for loaders/savers

--- tests/wekatests/coretests/converters.py
+++ tests/wekatests/coretests/converters.py
@@ -15,13 +15,76 @@
 # Copyright (C) 2014 Fracpete (pythonwekawrapper at gmail dot com)
 
 import unittest
+import os
 import weka.core.jvm as jvm
 import weka.core.converters as converters
 import wekatests.tests.weka_test as weka_test
 
 
 class TestConverters(weka_test.WekaTest):
-    pass
+
+    def test_loader_for_file(self):
+        """
+        Tests the loader_for_file method.
+        """
+        fname = "/some/where/anneal.arff"
+        loader = converters.loader_for_file(fname)
+        self.assertEqual("weka.core.converters.ArffLoader", loader.classname, msg="Class differs:  " + fname)
+        fname = "/some/where/anneal.arff.gz"
+        loader = converters.loader_for_file(fname)
+        self.assertEqual("weka.core.converters.ArffLoader", loader.classname, msg="Class differs: " + fname)
+        fname = "/some/where/anneal.csv"
+        loader = converters.loader_for_file(fname)
+        self.assertEqual("weka.core.converters.CSVLoader", loader.classname, msg="Class differs: " + fname)
+
+    def test_saver_for_file(self):
+        """
+        Tests the saver_for_file method.
+        """
+        fname = "/some/where/anneal.arff"
+        loader = converters.saver_for_file(fname)
+        self.assertEqual("weka.core.converters.ArffSaver", loader.classname, msg="Class differs:  " + fname)
+        fname = "/some/where/anneal.arff.gz"
+        loader = converters.saver_for_file(fname)
+        self.assertEqual("weka.core.converters.ArffSaver", loader.classname, msg="Class differs: " + fname)
+        fname = "/some/where/anneal.csv"
+        loader = converters.saver_for_file(fname)
+        self.assertEqual("weka.core.converters.CSVSaver", loader.classname, msg="Class differs: " + fname)
+
+    def test_arff_loader(self):
+        """
+        Tests the Loader class using an ArffLoader.
+        """
+        loader = converters.Loader(classname="weka.core.converters.ArffLoader")
+        data = loader.load_file(self.datadir() + os.sep + "anneal.arff")
+        self.assertIsNotNone(data)
+        self.assertEqual(898, data.num_instances, msg="Number of instances differs!")
+
+    def test_incremental_arff_loader(self):
+        """
+        Tests the Loader class using an incremental ArffLoader.
+        """
+        loader = converters.Loader(classname="weka.core.converters.ArffLoader")
+        data = loader.load_file(self.datadir() + os.sep + "anneal.arff", incremental=True)
+        self.assertIsNotNone(data)
+        count = 0
+        for inst in loader:
+            count += 1
+        self.assertEqual(898, count, msg="Number of instances differs!")
+
+    def test_arff_saver(self):
+        """
+        Tests the Saver class using an ArffSaver.
+        """
+        loader = converters.Loader(classname="weka.core.converters.ArffLoader")
+        data = loader.load_file(self.datadir() + os.sep + "anneal.arff")
+        self.assertIsNotNone(data)
+        outfile = self.tempfile("out.arff")
+        self.delfile(outfile)
+        saver = converters.Saver(classname="weka.core.converters.ArffSaver")
+        saver.save_file(data, outfile)
+        self.assertTrue(os.path.exists(outfile), "File does not exist: " + outfile)
+        self.delfile(outfile)
 
 
 def suite():
