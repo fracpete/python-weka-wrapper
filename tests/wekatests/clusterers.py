34f208b4c3537588891d079d369c1603133895ff
Peter Reutemann <fracpete@gmail.com>
2015-01-04 13:35:04 +1300

    added unit tests for clustering

--- tests/wekatests/clusterers.py
+++ tests/wekatests/clusterers.py
@@ -12,16 +12,156 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 # clusterers.py
-# Copyright (C) 2014 Fracpete (pythonwekawrapper at gmail dot com)
+# Copyright (C) 2014-2015 Fracpete (pythonwekawrapper at gmail dot com)
 
 import unittest
+import os
 import weka.core.jvm as jvm
+import weka.core.converters as converters
+import weka.core.dataset as dataset
+import weka.core.classes as classes
 import weka.clusterers as clusterers
+import weka.filters as filters
 import wekatests.tests.weka_test as weka_test
 
 
 class TestClusterers(weka_test.WekaTest):
-    pass
+
+    def test_instantiate_clusterer(self):
+        """
+        Tests the instantion of Clusterer objects.
+        """
+        cname = "weka.clusterers.SimpleKMeans"
+        cls = clusterers.Clusterer(classname=cname)
+        self.assertIsNotNone(cls, msg="Failed to instantiate clusterer!")
+        self.assertEqual(cname, cls.classname, msg="Classnames differ!")
+
+        cls = clusterers.FilteredClusterer()
+        self.assertIsNotNone(cls, msg="Failed to instantiate filtered clusterer!")
+        self.assertEqual("weka.clusterers.FilteredClusterer", cls.classname, msg="Classnames differ!")
+        cname = "weka.filters.unsupervised.attribute.Remove"
+        flter = filters.Filter(classname=cname)
+        self.assertEqual(cname, flter.classname, msg="Filter classnames differ!")
+        cls.filter = flter
+        self.assertEqual(cname, cls.filter.classname, msg="Filter classnames differ!")
+        cname = "weka.clusterers.EM"
+        cls.clusterer = clusterers.Clusterer(classname=cname)
+        self.assertEqual(cname, cls.clusterer.classname, msg="Base clusterer classnames differ!")
+
+    def test_build_clusterer(self):
+        """
+        Tests the build_clusterer class.
+        """
+        loader = converters.Loader(classname="weka.core.converters.ArffLoader")
+        data = loader.load_file(self.datadir() + os.sep + "anneal.arff")
+        self.assertIsNotNone(data)
+        data.delete_last_attribute()
+
+        # batch
+        cls = clusterers.Clusterer(classname="weka.clusterers.SimpleKMeans")
+        self.assertIsNotNone(cls, msg="Failed to instantiate clusterer!")
+        cls.build_clusterer(data)
+        self.assertIsNotNone(str(cls), msg="Failed to obtain string representation of model")
+
+        # incremental
+        cls = clusterers.Clusterer(classname="weka.clusterers.Cobweb")
+        self.assertIsNotNone(cls, msg="Failed to instantiate clusterer!")
+        cls.build_clusterer(dataset.Instances.template_instances(data))
+        for inst in data:
+            cls.update_clusterer(inst)
+        cls.update_finished()
+        self.assertIsNotNone(str(cls), msg="Failed to obtain string representation of model")
+
+    def test_cluster_instance(self):
+        """
+        Tests the cluster_instance class.
+        """
+        loader = converters.Loader(classname="weka.core.converters.ArffLoader")
+        data = loader.load_file(self.datadir() + os.sep + "anneal.arff")
+        self.assertIsNotNone(data)
+        data.delete_last_attribute()
+
+        cls = clusterers.Clusterer(classname="weka.clusterers.SimpleKMeans")
+        self.assertIsNotNone(cls, msg="Failed to instantiate clusterer!")
+        cls.build_clusterer(data)
+
+        for i in range(10):
+            cluster = cls.cluster_instance(data.get_instance(i))
+            self.assertIsNotNone(cluster, msg="Failed to cluster instance")
+
+    def test_distribution_for_instance(self):
+        """
+        Tests the distribution_for_instance method.
+        """
+        loader = converters.Loader(classname="weka.core.converters.ArffLoader")
+        data = loader.load_file(self.datadir() + os.sep + "anneal.arff")
+        self.assertIsNotNone(data)
+        data.delete_last_attribute()
+
+        cls = clusterers.Clusterer(classname="weka.clusterers.SimpleKMeans")
+        self.assertIsNotNone(cls, msg="Failed to instantiate clusterer!")
+        cls.build_clusterer(data)
+
+        for i in range(10):
+            dist = cls.distribution_for_instance(data.get_instance(i))
+            self.assertIsNotNone(dist, msg="Failed to obtain cluster membership for instance")
+            self.assertEqual(2, len(dist), msg="Number of clusters differs")
+
+    def test_clusterevaluation(self):
+        """
+        Tests the ClusterEvaluation class.
+        """
+        loader = converters.Loader(classname="weka.core.converters.ArffLoader")
+        data = loader.load_file(self.datadir() + os.sep + "anneal.arff")
+        self.assertIsNotNone(data)
+        data.delete_last_attribute()
+
+        # simple test set
+        test = dataset.Instances.copy_instances(data, 0, 10)
+        cls = clusterers.Clusterer(classname="weka.clusterers.SimpleKMeans")
+        self.assertIsNotNone(cls, msg="Failed to instantiate clusterer!")
+        cls.build_clusterer(data)
+
+        evl = clusterers.ClusterEvaluation()
+        self.assertIsNotNone(cls, msg="Failed to instantiate evaluation!")
+        evl.set_model(cls)
+        evl.test_model(test)
+        self.assertGreater(len(evl.cluster_results), 0, msg="No evaluation string!")
+        self.assertEqual(0.0, evl.log_likelihood, msg="log_likelihood differs")
+        self.assertEqual(2, evl.num_clusters, msg="num_clusters differs")
+        self.assertEquals(
+            [1., 0., 0., 1., 1., 1., 0., 0., 0., 0.], evl.cluster_assignments.tolist(),
+            msg="cluster_assignments differs")
+
+        # cross-validation
+        cls = clusterers.Clusterer(classname="weka.clusterers.EM", options=["-I", "3", "-X", "2", "-max", "5"])
+        self.assertIsNotNone(cls, msg="Failed to instantiate clusterer!")
+        llh = clusterers.ClusterEvaluation.crossvalidate_model(cls, data, 10, classes.Random(1))
+        self.assertAlmostEqual(-34.397, llh, places=3, msg="Failed to cross-validate clusterer!")
+
+    def test_classes_to_clusters(self):
+        """
+        Tests the classes_to_clusters method.
+        """
+        loader = converters.Loader(classname="weka.core.converters.ArffLoader")
+        data = loader.load_file(self.datadir() + os.sep + "anneal.arff")
+        self.assertIsNotNone(data)
+        #data.class_is_last()
+
+        train = dataset.Instances.copy_instances(data, 0, data.num_instances)
+        train.delete_last_attribute()
+
+        cls = clusterers.Clusterer(classname="weka.clusterers.SimpleKMeans")
+        self.assertIsNotNone(cls, msg="Failed to instantiate clusterer!")
+        cls.build_clusterer(train)
+
+        test = dataset.Instances.copy_instances(data, 0, data.num_instances)
+        test.class_is_last()
+        evl = clusterers.ClusterEvaluation()
+        self.assertIsNotNone(cls, msg="Failed to instantiate evaluation!")
+        evl.set_model(cls)
+        evl.test_model(test)
+        self.assertEqual([2, 4], evl.classes_to_clusters.tolist(), msg="classes to clusters differs")
 
 
 def suite():
