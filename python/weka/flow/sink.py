076be9795e7060bd7d67dcd0d4ee6a93f83cb19f
Peter Reutemann <fracpete@gmail.com>
2015-03-13 14:20:36 +1300

    added abstract FileOutputSink, DumpFile for string content and ModelWriter for Weka models

--- python/weka/flow/sink.py
+++ python/weka/flow/sink.py
@@ -15,7 +15,10 @@
 # Copyright (C) 2015 Fracpete (pythonwekawrapper at gmail dot com)
 
 
+import traceback
+import weka.core.serialization as serialization
 from weka.flow.base import InputConsumer
+from weka.flow.container import ModelContainer
 
 
 class Sink(InputConsumer):
@@ -42,7 +45,7 @@ class Sink(InputConsumer):
         """
         result = super(Sink, self).post_execute()
         if result is None:
-            self.input = None
+            self._input = None
         return result
 
 
@@ -136,3 +139,165 @@ class Console(Sink):
         """
         print(self.resolve_option("prefix") + str(self.input.payload))
         return None
+
+
+class FileOutputSink(Sink):
+    """
+    Ancestor for sinks that output data to a file.
+    """
+
+    def __init__(self, name=None, options=None):
+        """
+        Initializes the transformer.
+        :param name: the name of the transformer
+        :type name: str
+        :param options: the dictionary with the options (str -> object).
+        :type options: dict
+        """
+        super(FileOutputSink, self).__init__(name=name, options=options)
+
+    @property
+    def quickinfo(self):
+        """
+        Returns a short string describing some of the options of the actor.
+        :return: the info, None if not available
+        :rtype: str
+        """
+        return "output: '" + str(self.resolve_option("output")) + "'"
+
+    def fix_options(self, options):
+        """
+        Fixes the options, if necessary. I.e., it adds all required elements to the dictionary.
+        :param options: the options to fix
+        :type options: dict
+        :return: the (potentially) fixed options
+        :rtype: dict
+        """
+        options = super(FileOutputSink, self).fix_options(options)
+
+        opt = "output"
+        if opt not in options:
+            options[opt] = "."
+        if opt not in self.help:
+            self.help[opt] = "The file to write to (string)."
+
+        return options
+
+
+class DumpFile(FileOutputSink):
+    """
+    Sink that outputs the payloads of the data to a file.
+    """
+
+    def __init__(self, name=None, options=None):
+        """
+        Initializes the transformer.
+        :param name: the name of the transformer
+        :type name: str
+        :param options: the dictionary with the options (str -> object).
+        :type options: dict
+        """
+        super(DumpFile, self).__init__(name=name, options=options)
+
+    def description(self):
+        """
+        Returns a description of the actor.
+        :return: the description
+        :rtype: str
+        """
+        return "Sink that outputs the payloads of the data to a file."
+
+    @property
+    def quickinfo(self):
+        """
+        Returns a short string describing some of the options of the actor.
+        :return: the info, None if not available
+        :rtype: str
+        """
+        return super(DumpFile, self).quickinfo + ", append: " + str(self.resolve_option("append"))
+
+    def fix_options(self, options):
+        """
+        Fixes the options, if necessary. I.e., it adds all required elements to the dictionary.
+        :param options: the options to fix
+        :type options: dict
+        :return: the (potentially) fixed options
+        :rtype: dict
+        """
+        options = super(DumpFile, self).fix_options(options)
+
+        opt = "append"
+        if opt not in options:
+            options[opt] = False
+        if opt not in self.help:
+            self.help[opt] = "Whether to append to the file or overwrite (bool)."
+
+        return options
+
+    def do_execute(self):
+        """
+        The actual execution of the actor.
+        :return: None if successful, otherwise error message
+        :rtype: str
+        """
+        result = None
+        f = None
+        try:
+            if bool(self.resolve_option("append")):
+                f = open(str(self.resolve_option("output")), "a")
+            else:
+                f = open(str(self.resolve_option("output")), "w")
+            f.write(str(self.input.payload))
+            f.write("\n")
+        except Exception, e:
+            result = self.full_name + "\n" + traceback.format_exc()
+        finally:
+            if f is not None:
+                f.close()
+        return result
+
+
+class ModelWriter(FileOutputSink):
+    """
+    Writes a model to disk.
+    """
+
+    def __init__(self, name=None, options=None):
+        """
+        Initializes the transformer.
+        :param name: the name of the transformer
+        :type name: str
+        :param options: the dictionary with the options (str -> object).
+        :type options: dict
+        """
+        super(ModelWriter, self).__init__(name=name, options=options)
+
+    def description(self):
+        """
+        Returns a description of the actor.
+        :return: the description
+        :rtype: str
+        """
+        return "Writes a model to disk."
+
+    def check_input(self, token):
+        """
+        Performs checks on the input token. Raises an exception if unsupported.
+        :param token: the token to check
+        :type token: Token
+        """
+        if not isinstance(token.payload, ModelContainer):
+            raise Exception(self.full_name + ": Input token is not a ModelContainer!")
+
+    def do_execute(self):
+        """
+        The actual execution of the actor.
+        :return: None if successful, otherwise error message
+        :rtype: str
+        """
+        result = None
+        cont = self.input.payload
+        serialization.write_all(
+            str(self.resolve_option("output")),
+            [cont.get("Model").jobject, cont.get("Header").jobject])
+        return result
