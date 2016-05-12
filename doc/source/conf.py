76fb57cb41bd5acb43a1de79ea147a5c2e8c20c3
Peter Reutemann <fracpete@gmail.com>
2016-05-12 13:15:16 +1200

    updated autodoc

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -35,6 +35,14 @@ extensions = [
     'autoapi.sphinx'
 ]
 
+autoapi_modules = {
+   'weka': {
+      'override': False,
+      'output': 'auto'
+   }
+}
+
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
