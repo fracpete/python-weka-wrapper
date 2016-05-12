35d748c13c09563b806bc8a1637d6db83156fd28
Peter Reutemann <fracpete@gmail.com>
2016-05-12 14:40:27 +1200

    fixed typo in mock list

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -24,7 +24,7 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
             return Mock()
 
-MOCK_MODULES = ['javabridge, numpy']
+MOCK_MODULES = ['numpy', 'javabridge']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 
